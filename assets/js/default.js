233c0f667a295d3c66377a779c0b4d1950da7635
Matthew Gordon <laxredemption5525@gmail.com>
2017-05-12 13:36:37 -0600

    Add Flickity slider

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,48 +1,20 @@
-(function() {
+var flkty = new Flickity( '.main-carousel', {
+  cellAlign: 'left',
+  contain: true
+});
 
+(function() {
   'use strict';
+
   var toggles = document.querySelectorAll(".c-hamburger");
-  var nextArrow = document.getElementsByClassName("next");
-  var prevArrow = document.getElementsByClassName("previous");
-  var slides = document.querySelectorAll(".carousel-slide");
-  var fpItems = document.getElementsByClassName("fp-item center");
-  var arrows = document.getElementsByClassName("arrows");
-  var slideIndex = 1;
-  var direction;
+
+
+  console.log(flkty);
 
   for (let i = toggles.length - 1; i >= 0; i--) {
     var toggle = toggles[i];
     toggleHandler(toggle)
   };
-
-  window.onload = function() {
-    loadIndicators();
-    showSlidesFromRight(slideIndex);
-  }
-
-  nextArrow[0].addEventListener('click', function() {
-    direction = 'next';
-    console.log('next Clicked');
-    showSlidesFromRight(slideIndex += 1);
-    for (var i = 0; i < slides.length; i ++) {
-      slides[i].classList.remove("fadeLeft");
-      slides[i].classList.remove("fadeRight");
-      slides[i].className += " fadeRight";
-      fpItems[i].style.zIndex = 997;
-    }
-  });
-
-  prevArrow[0].addEventListener('click', function() {
-    direction = 'previous';
-    console.log('previous Clicked');
-    showSlidesFromRight(slideIndex -= 1);
-    for (var i = 0; i < slides.length; i ++) {
-      slides[i].classList.remove("fadeLeft");
-      slides[i].classList.remove("fadeRight");
-      slides[i].className += " fadeLeft";
-      fpItems[i].style.zIndex = 997;
-    }
-  });
 
   function toggleHandler(toggle) {
     let overlay = document.getElementById("overlay");
@@ -69,78 +41,9 @@
         clearMenuItems()
       }
 
-      // loadMenuItems();
     });
 
   }
-
-  function currentSlide(n) {
-    showSlidesFromRight(slideIndex = n);
-  }
-
-  function showSlidesFromRight(n) {
-    var dots = document.getElementsByClassName("dot");
-
-    if (n > slides.length) {
-      slideIndex = 1
-    }
-
-    if (n < 1) {
-      slideIndex = slides.length
-    }
-
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-      slides[i].className += " fadeRight"
-    }
-
-    for (let i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    slides[slideIndex - 1].style.display = "block";
-
-    dots[slideIndex - 1].className += " active";
-  }
-
-  function showSlidesFromLeft(n) {
-    var slides = document.getElementsByClassName("carousel-slide");
-    var dots = document.getElementsByClassName("dot");
-
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-
-    for (let i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    slides[slideIndex + 1].style.display = "block";
-    dots[slideIndex + 1].className += " active";
-  }
-
-  function loadIndicators() {
-    var count = 1;
-    var indicators = document.getElementsByClassName("indicators");
-
-    for (var i = 0; i < slides.length; i++) {
-      var indicator = document.createElement("span");
-
-      indicator.classList += "dot";
-      indicator.setAttribute("onclick", `currentSlide(${count})`);
-      indicators[0].appendChild(indicator);
-      count++;
-    }
-  }
-
 
   function loadMenuItem() {
     var menuItems = document.getElementsByClassName("navItem");
@@ -157,7 +60,6 @@
     icons[0].style.opacity = 1;
     icons[0].style.bottom = 0;
   }
-
 
   function clearMenuItems() {
     var menuItems = document.getElementsByClassName("navItem");
